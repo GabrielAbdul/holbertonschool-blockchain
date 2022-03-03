b6a276caf986678c9a33a9181e23e2959cfccb9f
Gabriel <Gabrielabdul1@gmail.com>
2022-03-03 19:44:43 +0000

    header file -- initial commit

---
+++ crypto/hblk_crypto.h
@@ -0,0 +1,62 @@
+#ifndef _HBLK_CRYPTO_
+#define _HBLK_CRYPTO_
+
+#include <openssl/bio.h>
+#include <openssl/buffer.h>
+#include <openssl/des.h>
+#include <openssl/evp.h>
+#include <openssl/pem.h>
+#include <openssl/rsa.h>
+#include <stdio.h>
+#include <openssl/sha.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+# define EC_CURVE   NID_secp256k1
+
+/* EC_KEY public key octet string length (using 256-bit curve) */
+# define EC_PUB_LEN     65
+/* Maximum signature octet string length (using 256-bit curve) */
+# define SIG_MAX_LEN    72
+
+# define PRI_FILENAME   "key.pem"
+# define PUB_FILENAME   "key_pub.pem"
+
+
+# define FILE_PATH_LEN 1024
+# define PUBKEY 0
+# define PRIVKEY 1
+
+/**
+ * struct sig_s - EC Signature structure
+ *
+ * @sig: Signature buffer. The whole space may not be used
+ * @len: signature size. Can't exceed SIG_MAX_LEN, therefore stored on a byte
+ */
+typedef struct sig_s
+{
+	/*
+	 * @sig must stay first, so we can directly use the structure as
+	 * an array of char
+	 */
+	uint8_t     sig[SIG_MAX_LEN];
+	uint8_t     len;
+} sig_t;
+/* Task prototypes */
+uint8_t *sha256(int8_t const *s, size_t len,
+				uint8_t digest[SHA256_DIGEST_LENGTH]);
+EC_KEY *ec_create(void);
+uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
+EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
+int ec_save(EC_KEY *key, char const *folder);
+EC_KEY *ec_load(char const *folder);
+uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg
+				size_t msglen, sig_t *sig);
+int ec_verify(EC_KEY const *key, uint8_t const *msg,
+				size_t msglen, sig_t const *sig);
+
+/* Helper prototypes */
+int save_key_to_file(EC_KEY *key, char const *folder, int key_type);
+int load_key_from_file(EC_KEY **keys, char const *folder, int key_type);
+#endif /* _HBLK_CRYPTO_ */
