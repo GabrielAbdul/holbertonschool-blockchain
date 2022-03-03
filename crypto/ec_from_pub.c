3d97da387fa28fdb1f756056b3e7db4414ec2f01
Gabriel <Gabrielabdul1@gmail.com>
2022-03-03 20:04:23 +0000

    creates an EC_KEY structure given a public key -- accounts for NULL input

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -11,6 +11,9 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 	const EC_GROUP *curve;
 	EC_POINT *public_key;
 
+	if (pub == NULL)
+		return (NULL);
+		
 	/* create EC_KEY structure based on curve*/
 	keys = EC_KEY_new_by_curve_name(EC_CURVE);
 	/* retrieve curve from newly created key struct */
