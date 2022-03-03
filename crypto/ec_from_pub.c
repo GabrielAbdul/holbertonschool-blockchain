736dd85a5c7a99d8f90070ec7e36cfee081f04ee
Gabriel <Gabrielabdul1@gmail.com>
2022-03-03 20:07:11 +0000

    creates an EC_KEY structure given a public key -- trailing whitespace fix

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -13,7 +13,7 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 
 	if (pub == NULL)
 		return (NULL);
-		
+
 	/* create EC_KEY structure based on curve*/
 	keys = EC_KEY_new_by_curve_name(EC_CURVE);
 	/* retrieve curve from newly created key struct */
