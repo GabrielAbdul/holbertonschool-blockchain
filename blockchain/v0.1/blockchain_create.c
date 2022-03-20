18229b11c310f75a96800d2853515e00dc28a533
Gabriel <Gabrielabdul1@gmail.com>
2022-03-20 17:51:38 +0000

    init data to 0 before memseting it

--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -1,17 +1,17 @@
 #include "blockchain.h"
 
+
 /**
- * blockchain_create - creates Genesis block of Blockchain
+ * blockchain_create - creates block block of Blockchain
  * Return: newly initialized Blockchain structure
+ * TODO ADD COMMENTS, UPDATE FUNCTION DOCS
  */
 blockchain_t *blockchain_create(void)
 {
 	block_t *block;
-	blockchain_t *blockchain;
-	char *hash = "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03"; /* c52c26c8b5461639635d8edf2a97d48d0c8e0009c817f2b1d3d7ff2f04515803 */
+	blockchain_t *blockchain = calloc(1, sizeof(blockchain_t));
 
 	block = calloc(1, sizeof(block_t));
-	blockchain = malloc(sizeof(blockchain_t));
 
 	if (block == NULL || blockchain == NULL)
 	{
@@ -31,14 +31,16 @@ blockchain_t *blockchain_create(void)
 
 	block->info.index = 0;
 	block->info.difficulty = 0;
-	block->info.timestamp = 1537578000;
+	block->info.timestamp = TIMESTAMP;
 	block->info.nonce = 0;
 	memset(block->info.prev_hash, 0, sizeof(block->info.prev_hash));
 
-	memcpy(&(block->data.buffer), "Holberton School", 16);
-	block->data.len = 16;
-	memcpy(&(block->hash), hash, 256);
+	block->data.len = DATA_LEN;
+	memset(block->data.buffer, 0, sizeof(block->data.buffer));
+	memcpy(block->data.buffer, DATA, block->data.len);
 
+	memset(block->hash, 0, sizeof(HASH));
+	memcpy(block->hash, HASH, sizeof(HASH));
 	if (llist_add_node(blockchain->chain, block, ADD_NODE_FRONT) == -1)
 	{
 		llist_destroy(blockchain->chain, KEEP_NODES, NULL);
@@ -46,5 +48,6 @@ blockchain_t *blockchain_create(void)
 		free(blockchain);
 		return (NULL);
 	}
+
 	return (blockchain);
 }
