ca599adbb6947b115f791d555ba66f9328a82ff7
Gabriel <Gabrielabdul1@gmail.com>
2022-04-23 18:14:08 +0000

    kernel style coding fix

--- blockchain/v0.3/block_destroy.c
+++ blockchain/v0.3/block_destroy.c
@@ -6,9 +6,9 @@
  */
 void block_destroy(block_t *block)
 {
-    if (block == NULL)
-        return;
+	if (block == NULL)
+		return;
 
-    llist_destroy(block->transactions, 1, (node_dtor_t)transaction_destroy);
-    free(block);
+	llist_destroy(block->transactions, 1, (node_dtor_t)transaction_destroy);
+	free(block);
 }
