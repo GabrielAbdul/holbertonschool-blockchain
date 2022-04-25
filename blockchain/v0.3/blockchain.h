fb54170eebbc8257ce25f6ceb9bce3bfac7f5175
Gabriel <Gabrielabdul1@gmail.com>
2022-04-25 16:41:04 +0000

    header

--- blockchain/v0.3/blockchain.h
+++ blockchain/v0.3/blockchain.h
@@ -24,7 +24,7 @@
 
 #include "../../crypto/hblk_crypto.h"
 #include "provided/endianness.h"
-#include "transaction.h"
+#include "transaction/transaction.h"
 
 #define HASH \
 "\
@@ -53,8 +53,8 @@
  */
 typedef struct blockchain_s
 {
-    llist_t     *chain;
-    llist_t     *unspent;
+	llist_t     *chain;
+	llist_t     *unspent;
 } blockchain_t;
 
 
@@ -109,12 +109,29 @@ typedef struct block_info_s
  */
 typedef struct block_s
 {
-    block_info_t    info; /* This must stay first */
-    block_data_t    data; /* This must stay second */
-    llist_t     *transactions;
-    uint8_t     hash[SHA256_DIGEST_LENGTH];
+	block_info_t    info; /* This must stay first */
+	block_data_t    data; /* This must stay second */
+	llist_t     *transactions;
+	uint8_t     hash[SHA256_DIGEST_LENGTH];
 } block_t;
 
+/* GENESIS BLOCK - first block in the chain */
+#define GENESIS_BLOCK { \
+	{ /* info */ \
+		0 /* index */, \
+		0, /* difficulty */ \
+		1537578000, /* timestamp */ \
+		0, /* nonce */ \
+		{0} /* prev_hash */ \
+	}, \
+	{ /* data */ \
+		"Holberton School", /* buffer */ \
+		16 /* len */ \
+	}, \
+	NULL, /* transactions */ \
+	"\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d" \
+	"\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03" \
+}
 
 
 blockchain_t *blockchain_create(void);
@@ -127,6 +144,8 @@ uint8_t *block_hash(block_t const *block,
 int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
 							uint32_t difficulty);
 void block_mine(block_t *);
-int block_is_valid(block_t const *block, block_t const *prev_block);
 uint32_t blockchain_difficulty(blockchain_t const *blockchain);
+int validate_tx(llist_node_t node, unsigned int idx, void *arg);
+int block_is_valid(block_t const *block, block_t const *prev_block,
+	llist_t *all_unspent);
 #endif /* BLOCKCHAIN_H */
