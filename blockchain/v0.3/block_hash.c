c364698ea1c0ccc79f479833c016a813be4c2e57
Gabriel <Gabrielabdul1@gmail.com>
2022-04-25 16:23:10 +0000

    function block_hash to include the list of transactions in a Block’s hash

--- blockchain/v0.3/block_hash.c
+++ blockchain/v0.3/block_hash.c
@@ -2,15 +2,42 @@
 
 /**
  * block_hash - computes the hash of a Block
- * @block: points to the Block to be hashed
- * @hash_buf: buf to store hash
- * Return: pointer to @hash_buf
- */
-uint8_t *block_hash(block_t const *block,
-	uint8_t hash_buf[SHA256_DIGEST_LENGTH])
+ * @block: pointer to block to be hashed
+ * @hash_buf: where hash will be stored
+ * Return: pointer to hash_buf
+ **/
+uint8_t
+*block_hash(block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
-	size_t len = sizeof(block->info) + block->data.len;
+	size_t len;
+	uint8_t *buf;
+	int i, size;
+	transaction_t *tmp_tx;
 
-	sha256((int8_t const *)block, len, hash_buf);
+	if (!block || !hash_buf)
+		return (NULL);
+
+	if (block->transactions)
+		size = llist_size(block->transactions);
+	else
+		size = 0;
+	len = sizeof(block->info) + block->data.len;
+	buf = calloc(1, len + (size * sizeof(tmp_tx->id)));
+	memcpy(buf, block, len);
+
+	for (i = 0 ; i < size; i++)
+	{
+		tmp_tx = llist_get_node_at(block->transactions, i);
+		memcpy(buf + len, tmp_tx->id, sizeof(tmp_tx->id));
+		len += sizeof(tmp_tx->id);
+	}
+
+	if (!sha256((int8_t const *)buf, len, hash_buf))
+	{
+		free(buf);
+		return (NULL);
+	}
+
+	free(buf);
 	return (hash_buf);
 }
