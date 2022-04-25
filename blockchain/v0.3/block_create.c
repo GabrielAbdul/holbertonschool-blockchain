adf947be62d87d03f95a4cc5ef4588c8e1efd5de
Gabriel <Gabrielabdul1@gmail.com>
2022-04-25 16:20:00 +0000

    now initializes the Block’s transaction list to an empty linked list

--- blockchain/v0.3/block_create.c
+++ blockchain/v0.3/block_create.c
@@ -1,30 +1,25 @@
 #include "blockchain.h"
 
-block_t *block_create(block_t const *prev,
-					  int8_t const *data, uint32_t data_len)
+/**
+ * block_create - creates a new block in the sequence
+ * @prev: pointer to previous block
+ * @data: pointer of data to duplicate
+ * @data_len: length of data
+ * Return: pointer to new block or NULL
+ */
+block_t *block_create(block_t const *prev, int8_t const *data,
+	uint32_t data_len)
 {
-	block_t *block = calloc(1, sizeof(block_t));
+	block_t *block = calloc(1, sizeof(*block));
+	llist_t *transactions = llist_create(MT_SUPPORT_FALSE);
 
-	if (block == NULL)
-		return (NULL);
-
-	data_len = (data_len > BLOCKCHAIN_DATA_MAX) ? BLOCKCHAIN_DATA_MAX : data_len;
-	memcpy(block->data.buffer, data, data_len);
-	block->data.len = data_len;
-	block->info.difficulty = 0;
-	block->info.nonce = 0;
-	time((time_t *)&block->info.timestamp);
-	if (prev)
-	{
-		block->info.index = prev->info.index + 1;
-		memcpy(block->info.prev_hash, prev->hash, sizeof(prev->hash));
-	} else
-	{
-		block->info.index = 0;
-		memset(block->info.prev_hash, 0, SHA256_DIGEST_LENGTH);
-	}
-	block->transactions = llist_create(MT_SUPPORT_FALSE);
-	memset(block->hash, 0, sizeof(block->hash));
-
+	if (!block || !transactions)
+		return (free(block), llist_destroy(transactions, 0, NULL), NULL);
+	memcpy(&(block->data.buffer), data, MIN(data_len, BLOCKCHAIN_DATA_MAX));
+	block->data.len = MIN(data_len, BLOCKCHAIN_DATA_MAX);
+	block->info.index = prev->info.index + 1;
+	block->info.timestamp = time(NULL);
+	block->transactions = transactions;
+	memcpy(&(block->info.prev_hash), prev->hash, SHA256_DIGEST_LENGTH);
 	return (block);
 }
